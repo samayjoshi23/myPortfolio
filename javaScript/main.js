0a80605ffde47323925bf3d1a397c532dd520076
Samay Joshi <63327946+samayjoshi23@users.noreply.github.com>
2022-01-13 21:14:35 +0530

    Qualification
    Qualification and work completed with Js code.

--- javaScript/main.js
+++ javaScript/main.js
@@ -40,27 +40,41 @@ skillsHeaders.forEach(header => {
     });
 });
 
-
-
-
-
-// const skillsContent = document.querySelectorAll('.skills__content');
-// const skillsHeader = document.querySelectorAll('.skills__header');
-
-// function toggleSkills(){
-//     let itemClass = this.parentNode.className;
-//     for( let i = 0 ; i < skillsContent ; i++){
-//         skillsContent[i].className = 'skills__content skills__close';
-//     }
-//     if(itemClass === 'skills__content skills__close'){
-//         this.parentNode.className = 'skills__content skills__open'
-//     }
-// }
-// skillsHeader.forEach(header =>{
-//     header.addEventListener('click', toggleSkills);
-// })
 /*==================== QUALIFICATION TABS ====================*/
+const selectedBtn = document.querySelectorAll('.qualification__button');
+const selectedSection = document.querySelectorAll('.qualification__content');
 
+selectedBtn.forEach((btn, index) => {
+    btn.addEventListener("click", function () {
+      selectedBtn.forEach((section, i) => {
+        if (section.classList.contains("qualification__activeBtn")) {
+          section.classList.remove("qualification__activeBtn");
+          selectedSection[i].classList.remove("qualification__active");
+        }
+      });
+      btn.classList.add("qualification__activeBtn");
+      selectedSection[index].classList.add("qualification__active");
+    });
+  });
+
+// const tabs = document.querySelectorAll('[data-target]');
+// const tabContents = document.querySelectorAll('[data-content]');
+// 
+// tabs.forEach(tab =>{
+//     tab.addEventListener('click', ()=>{
+//         const target = document.querySelector(tab.dataset.target);
+
+//         tabContents.forEach(tabContent => {
+//             tabContent.classList.remove('qualification__active');
+//         })
+//         target.classList.add('qualification__active');
+
+//         tab.forEach(tab=>{
+//             tab.classList.remove('qualification__active');
+//         })
+//         tab.classList.add('qualification__active');
+//     });
+// });
 
 /*==================== SERVICES MODAL ====================*/
 
